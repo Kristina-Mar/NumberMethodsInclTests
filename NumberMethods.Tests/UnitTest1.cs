3a4601f7a9f96f4a507aaeb4f16ae5fd8ca9d966
Kristina Martiskova <kris.martiskova@gmail.com>
2024-02-24 11:15:52 +0100

    Added one new case to each unit test (different array).

--- NumberMethods.Tests/UnitTest1.cs
+++ NumberMethods.Tests/UnitTest1.cs
@@ -9,6 +9,10 @@ namespace NumberMethods.Tests
             int[] numbers = { 1, 5, 8, 7, 98, 35, 25, 4, 11, 15, 58, 56, 32 };
             numberMethods.ReturnLowestNumber(numbers);
             Assert.Equal(1, numberMethods.LowestNumber);
+
+            int[] numbers2 = { 2, 2, 4 };
+            numberMethods.ReturnLowestNumber(numbers2);
+            Assert.Equal(2, numberMethods.LowestNumber);
         }
         [Fact]
         public void TestReturnHighestNumber()
@@ -17,6 +21,10 @@ namespace NumberMethods.Tests
             int[] numbers = { 1, 5, 8, 7, 98, 35, 25, 4, 11, 15, 58, 56, 32 };
             numberMethods.ReturnHighestNumber(numbers);
             Assert.Equal(98, numberMethods.HighestNumber);
+
+            int[] numbers2 = { 2, 2, 4 };
+            numberMethods.ReturnHighestNumber(numbers2);
+            Assert.Equal(4, numberMethods.HighestNumber);
         }
         [Fact]
         public void TestReturnAverage()
@@ -25,6 +33,10 @@ namespace NumberMethods.Tests
             int[] numbers = { 1, 5, 8, 7, 98, 35, 25, 4, 11, 15, 58, 56, 32 };
             numberMethods.ReturnAverage(numbers);
             Assert.Equal(27.31, numberMethods.Average);
+
+            int[] numbers2 = { 2, 2, 4 };
+            numberMethods.ReturnAverage(numbers2);
+            Assert.Equal(2.67, numberMethods.Average);
         }
     }
 }
